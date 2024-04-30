6e89435c9f4fd1ee23bafbb4173f7d52c5f9cdfb
Marc Hernández <marc.hernandez@vasscompany.com>
2024-04-30 22:52:55 +0200

    Added Fragments and Bottom Navigation listener.

--- app/src/main/java/com/marchernandez/snapshots/MainActivity.kt
+++ app/src/main/java/com/marchernandez/snapshots/MainActivity.kt
@@ -5,16 +5,75 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.marchernandez.snapshots.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mbinding: ActivityMainBinding
+
+    private lateinit var mActiveFragment: Fragment
+    private lateinit var mFragmentManager: FragmentManager
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mbinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mbinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        setupBottomNav()
+    }
+
+    private fun setupBottomNav() {
+        mFragmentManager = supportFragmentManager
+
+        val homeFragment = HomeFragment()
+        val addFragment = AddFragment()
+        val profileFragment = ProfileFragment()
+
+        mActiveFragment = homeFragment
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, profileFragment, ProfileFragment::class.java.name)
+            .hide(profileFragment).commit()
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, addFragment, AddFragment::class.java.name).hide(addFragment)
+            .commit()
+
+        mFragmentManager.beginTransaction()
+            .add(R.id.hostFragment, homeFragment, HomeFragment::class.java.name).commit()
+
+        mbinding.bottomNav.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.action_home -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(homeFragment)
+                        .commit()
+                    mActiveFragment = homeFragment
+                    true
+                }
+
+                R.id.action_add -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(addFragment)
+                        .commit()
+                    mActiveFragment = addFragment
+                    true
+                }
+
+                R.id.action_profile -> {
+                    mFragmentManager.beginTransaction().hide(mActiveFragment).show(profileFragment)
+                        .commit()
+                    mActiveFragment = profileFragment
+                    true
+                }
+
+                else -> false
+            }
         }
     }
 }
